0f4f95f2b3e73eaefc30ca74dec11ce0f3be9ff6
LucaTosetti01 <47659781+LucaTosetti01@users.noreply.github.com>
2019-04-28 16:14:59 +0200

    Update main.java

--- main.java
+++ main.java
@@ -28,11 +28,11 @@ public class EsSabbia_Scatola extends PApplet {
      */
     public static void main(String[] args) {
         //numBalls = 2;
-        ptrDati= new DatiCondivisi(640,320,100);
-        Scatole[] array = ptrDati.getArray();
+        ptrDati= new DatiCondivisi(900,750,100);
+        array = ptrDati.getArray();
         s = new Sensore();
 
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 2; i++) {
             if (i == 0) {
                 array[i] = new Scatole(3375, 100, i, false, ptrDati, 150, 150, 150);
             } else {
@@ -46,7 +46,7 @@ public class EsSabbia_Scatola extends PApplet {
     }
 
     public void settings() {
-        //size(box1.getLungS(), box1.getAltS());
+        size(ptrDati.getLungS(), ptrDati.getAltS());
         //ptrDati.setScreen(width, height);
         /*for (int i = 0; i < thBalls.length; i++) {
             thBalls[i].start();
@@ -66,7 +66,7 @@ public class EsSabbia_Scatola extends PApplet {
 
         // clean the screen
         background(119, 136, 153);
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 2; i++) {
             drawBox(i);
         }
 
@@ -75,6 +75,7 @@ public class EsSabbia_Scatola extends PApplet {
     public void drawBox(int id) {
         int lunB1 = array[id].getLungB();
         int altB1 = array[id].getAltB();
+        
         int lunB2 = array[id].getLungB();
         int altB2 = array[id].getAltB();
         int lunS = ptrDati.getLungS();
@@ -83,11 +84,21 @@ public class EsSabbia_Scatola extends PApplet {
         stroke(0, 0, 0);
 
         fill(color(202, 188, 145, array[id].valueSand()));
-
-        rect((lunS / 2), ((altS / 2) - (altB2 / 2)), lunB2, altB2);
+        
+        for(int i=0;i<900;i=i+150) {
+            for(int ii=0;ii<750;i=i+150) {
+                if(i==)
+                rect(i,ii,lunB1,altB1);
+            }
+        }
+        
+        
+        //rect(((lunS / 2) - lunB1), ((altS / 2) - (altB1 / 2)), lunB1, altB1);
 
        // fill(color(202, 188, 145, box1.valueSand()));
-        rect(((lunS / 2) - lunB1), ((altS / 2) - (altB1 / 2)), lunB1, altB1);
+        
+        
+        //rect((lunS / 2), ((altS / 2) - (altB2 / 2)), lunB2, altB2);
     }
 
 }
