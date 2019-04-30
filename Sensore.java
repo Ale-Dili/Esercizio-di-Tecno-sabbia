1ab57d725362f01055f29486d7bdd8b6c04a9201
chri-sp <47659802+chri-sp@users.noreply.github.com>
2019-04-30 15:33:27 +0200

    Add files via upload

--- Sensore.java
+++ Sensore.java
@@ -20,18 +20,18 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 public class Sensore {
-    
+
     private JFrame frame;
     float inclinazioneX;
     float inclinazioneY;
-    
+
     public Sensore() {
         this.inclinazioneX = 0;
         this.inclinazioneY = 0;
     }
-    
+
     public Sensore(DatiCondivisi datiC) {
-        
+
         frame = new JFrame("Controls");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
@@ -56,7 +56,7 @@ public class Sensore {
                 slider.setValue((int) (datiC.getInclinazioneY() - 1));
             }
         });
-        
+
         slider.setMaximum(90);
         slider.setMinimum(-90);
         slider.setValue(0);
@@ -80,31 +80,31 @@ public class Sensore {
         // add the buttons to the panel      
         controls.add(AddValue);
         controls.add(DecValue);
+        controls.add(resetB);
         controls.add(slider);
         controls.add(valore);
-        controls.add(resetB);
-        
+
         // add the panel to the frame
         frame.add(controls);
-        frame.setSize(400, 150);
+        frame.setSize(400, 120);
     }
-    
+
     public void setInclinazioneX(float inclinazioneX) {
         this.inclinazioneX = inclinazioneX;
     }
-    
+
     public void setInclinazioneY(float inclinazioneY) {
         this.inclinazioneY = inclinazioneY;
     }
-    
+
     public float getInclinazioneX() {
         return inclinazioneX;
     }
-    
+
     public float getInclinazioneY() {
         return inclinazioneY;
     }
-    
+
     public void show() {
         frame.setVisible(true);
     }
