672923726c5750ccdd89a8feacc5386462730614
LucaTosetti01 <47659781+LucaTosetti01@users.noreply.github.com>
2019-04-30 16:29:32 +0200

    Update ThScatola.java

--- ThScatola.java
+++ ThScatola.java
@@ -6,6 +6,11 @@
 package es.sabbia_scatola;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import processing.core.*;
 
 /**
@@ -33,28 +38,87 @@ public class ThScatola extends Thread {
         while (true) {
             array[id].move();
             array[id].setSabbiaPersa(array[id].getSabbiaSpostata());
-            if (array[id].getSabbiaPersa() > 0) {
-                float persa = array[id].getSandQuantity() - array[id].getSabbiaPersa();
-                array[id].setSandQuantity(persa);
+            array[id].setSabbiaSpostata();
+            
+            
+            try {
+                Thread.sleep(10);                        //provare 5 millisecondi
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ThScatola.class.getName()).log(Level.SEVERE, null, ex);
+            }
 
-                if (ptrDati.getInclinazioneY() > 0) {
-                    if (id == 1) {
-                        array[id].setIdTarget(id + 1);
-                        array[array[id].getIdTarget()].setSandQuantity(array[array[id].getIdTarget()].getSandQuantity() + array[id].getSabbiaPersa());
-                    }//
+            if (ptrDati.getInclinazioneY() > 0) {
+                
+                array[id].getBall().IncrementaVelocitàX();
+                
+                if (id == 0) {
+                    if (array[id].getSabbiaPersa() > 0) {
+
+                        float newQuantity = array[id].getSandQuantity() - array[id].getSabbiaPersa();
+                        if (newQuantity < 0) {
+                            newQuantity = 0;
+                            array[id].setSandQuantity(newQuantity);
+                            array[id].setPerSabbia(0);
+                            array[array[id].getIdTarget()].setPerSabbia(100);
+                            array[array[id].getIdTarget()].setSandQuantity(3375);
+                            
+                            
+                            
+                        } else {
+                            array[id].setSandQuantity(newQuantity);
+                            array[id].setPerSabbia((int) (array[id].getSandQuantity() * 100) / 3375);                                         //100 : 3375 = x : sandQuantity
+                            //System.out.println("prova:1 sandQuantity "+array[id].getSandQuantity());      //OUTPUT DI PROVA
+                            array[id].setIdTarget(id + 1);
+                            array[array[id].getIdTarget()].setSandQuantity(array[array[id].getIdTarget()].getSandQuantity() + array[id].getSabbiaPersa());
+                            array[array[id].getIdTarget()].setPerSabbia((int) (array[array[id].getIdTarget()].getSandQuantity() * 100) / 3375);
+                            
+                            
+                            
+                        }
+                    }
                 }
-                if (ptrDati.getInclinazioneY() > 0) {
-                    if (id == 1) {
-                        array[id].setIdTarget(id - 1);
-                        array[array[id].getIdTarget()].setSandQuantity(array[array[id].getIdTarget()].getSandQuantity() + array[id].getSabbiaPersa());
+            }
+            if (ptrDati.getInclinazioneY() < 0) {
+                
+                array[id].getBall().DecrementaVelocitàX();
+                
+                if (id == 1) {
+
+                    if (array[id].getSabbiaPersa() > 0) {
+
+                        float newQuantity = array[id].getSandQuantity() - array[id].getSabbiaPersa();
+                        
+                        if ((newQuantity < 0) && (array[id].getIdTarget()!=-1)) {
+                            newQuantity = 0;
+                            array[id].setSandQuantity(newQuantity);
+                            array[id].setPerSabbia(0);
+                            array[array[id].getIdTarget()].setPerSabbia(100);
+                            array[array[id].getIdTarget()].setSandQuantity(3375);
+                            
+                            
+                        } else {
+                            array[id].setSandQuantity(newQuantity);
+                            array[id].setPerSabbia((int) (array[id].getSandQuantity() * 100) / 3375);                                         //100 : 3375 = x : sandQuantity
+                            //System.out.println("prova:1 sandQuantity "+array[id].getSandQuantity());      //OUTPUT DI PROVA
+                            array[id].setIdTarget(id - 1);
+                            array[array[id].getIdTarget()].setSandQuantity(array[array[id].getIdTarget()].getSandQuantity() + array[id].getSabbiaPersa());
+                            array[array[id].getIdTarget()].setPerSabbia((int) (array[array[id].getIdTarget()].getSandQuantity() * 100) / 3375);
+                            
+                            
                     } //
+                    }
                 }
-                array[id].setSabbiaPersa(0);
+
             } else {
                 //
             }
+            array[id].moveBall();
+            
+            array[id].setSabbiaPersa(0);
+            ptrDati.setArray(array);
             //-------------------------------------------------------------SETTARE NUOVAMENTE L'ARRAY
         }
+
     }
 
     /* public int getSabbiaPersa() {
@@ -70,40 +134,53 @@ public class ThScatola extends Thread {
     /*  public int getLungS() {
         return lungS;
     }
+
     public int getAltS() {
         return altS;
     }
+
     public int getLungB() {
         return lungB;
     }
+
     public int getAltB() {
         return altB;
     }
+
     public void setLungS(int lungS) {
         this.lungS = lungS;
     }
+
     public void setAltS(int altS) {
         this.altS = altS;
     }
+
     public void setLungB(int lungB) {
         this.lungB = lungB;
     }
+
     public void setAltB(int altB) {
         this.altB = altB;
     }
+
     public int getPerSabbia() {
         return perSabbia;
     }
+
     public float getInclinazione() {
         return inclinazione;
     }
+
     public void setPerSabbia(int perSabbia) {
         this.perSabbia = perSabbia;
     }
+
     public void setInclinazione(float inclinazione) {
         this.inclinazione = inclinazione;
     }
+
     public int valueSand() {
         return (255 * perSabbia) / 100;
+
     }*/
 }
